93566baefc3feaebdf8cdcc6e4e1e6cafcda9f13
fguira <francisco.guiraldelli@gmail.com>
2016-10-29 22:28:48 -0200

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20161029172852) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "anuncios", force: :cascade do |t|
     t.string   "titulo"
     t.text     "descricao"
